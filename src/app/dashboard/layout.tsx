40016d96e2f26fa5a48a32050b8f8422a69ff994
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-07-07 18:26:31 +0600

    changed the codes

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 
-const layout = ({ children }: { children: React.ReactNode }) => {
+interface DashboardLayoutProps {
+  children?: React.ReactNode;
+}
+
+const layout = (props: DashboardLayoutProps) => {
   return (
     <div>
-      <h1>nav</h1>
-      <div>side bar</div>
-      {children}
+      <nav className="bg-black text-white py-4 px-5  justify-between items-center">
+        <h3 className="text-xl">Logo</h3>
+        <ul className="flex items-center gap-8">
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Login</li>
+        </ul>
+      </nav>
+      <main>{props.children}</main>
     </div>
   );
 };
