68977a3dae4a0040cf95eb1aa8d24a1d24a0a636
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-07-07 18:08:23 +0600

    added dashboard

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DashboardPage: React.FC = (props) => {
+  return (
+    <>
+      <div>I am a dashboard</div>
+    </>
+  );
+};
+
+export default DashboardPage;
