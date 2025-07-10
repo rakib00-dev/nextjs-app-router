c5343a912edca5c35ebbb1a7e34db985cd3d54b9
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-07-10 10:47:45 +0600

    added server and client compoenets together

--- src/app/dashboard/todos/page.tsx
+++ src/app/dashboard/todos/page.tsx
@@ -1,3 +1,5 @@
+import Button from '@/components/Button';
+
 async function fetchAllData() {
   const res = await fetch('https://jsonplaceholder.typicode.com/users', {
     cache: 'no-cache',
@@ -7,9 +9,11 @@ async function fetchAllData() {
 
 const TodosPage = async () => {
   const todos = await fetchAllData();
+
   return (
     <>
       <div className="text-4xl">I am a todosPage</div>
+      <Button />
       {todos &&
         todos?.map((e: any) => (
           <div
