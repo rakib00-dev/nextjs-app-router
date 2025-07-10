54cccfee581cc2b62bf84adfb5c26353e99b84eb
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-07-10 10:00:28 +0600

    added pages

---
+++ src/app/dashboard/todos/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TodosPage: React.FC = (props) => {
+  return (
+    <>
+      <div className="text-4xl">I am a todosPage</div>
+    </>
+  );
+};
+
+export default TodosPage;
---
+++ src/app/dashboard/users/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UsersPage: React.FC = (props) => {
+  return (
+    <>
+      <div className="text-4xl">I am a UsersPage</div>
+    </>
+  );
+};
+
+export default UsersPage;
